897a8146c10a5199a1352acbb55f9b5ee358dfc0
svarona <sarai.var.fer@gmail.com>
2019-12-18 09:07:37 +0100

    Created script to merge FPKMs

--- bin/merge_FPKM.R
+++ bin/merge_FPKM.R
@@ -5,3 +5,8 @@ args <- commandArgs(trailingOnly=TRUE)
 
 require(ballgown)
 my.data = ballgown(dataDir="./", samplePattern='*_ballgown', meas='all')
+expression <- gexpr(my.data)
+expression <- as.data.frame(expression)
+expression$gene <- rownames(expression)
+expression <- expression[, c(ncol(expression), 1:c(ncol(expression)-1))]
+write.table(x = expression, file = "merged_FPKM.txt", col.names = T, row.names = F, sep = "\t", quote = F)
